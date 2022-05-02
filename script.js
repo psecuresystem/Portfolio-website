d59d0bcb7ef169c3001d243880909b91f108b516
Vision Onyeaku <vision.onyeaku@zummitafria.com>
2022-05-03 00:36:05 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let sections = document.querySelectorAll('.section')
+let sectBtns = document.querySelectorAll('.controls')
+let sectBtn = document.querySelectorAll('.control')
+let main = document.querySelector('main')
+
+function PageTransitions(){
+    //Button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click',e => {
+            let currentButton = document.querySelector('.active-btn')
+            currentButton.classList.remove('active-btn')
+            e.target.classList.add('active-btn')
+            let id = sectBtn[i].getAttribute('data-id')
+            let active = document.querySelector('.section.active')
+            let activeSection = document.querySelector(`#${id}`)
+            active.classList.remove('active')
+            activeSection.classList.add('active')
+        })
+    }
+}
+
+PageTransitions()
